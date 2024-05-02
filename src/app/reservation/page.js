2c4e84557374b62fbe10b0f39fade3b1f433b641
6dean <ferdjallahrockdean@gmail.com>
2024-05-02 18:12:14 +0200

    erreur img

--- src/app/reservation/page.js
+++ src/app/reservation/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Booking() {
   return (
@@ -6,6 +7,7 @@ export default function Booking() {
       <div>
         <Link href="/">ici tu reserves</Link>
       </div>
+      <Image src="/public/images/0002.jpg" width={100} height={100} alt="car" />
     </main>
   );
 }
